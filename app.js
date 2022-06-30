372c2f5a3ec81e7584602b6adddd10d5a5e16287
chh817 <chh817@gmail.com>
2022-07-01 00:20:27 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const session = require('express-session')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
-const PORT = 3000
+const PORT = 3000 || process.env.PORT
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 const routes = require('./routes')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
