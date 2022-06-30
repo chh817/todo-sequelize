62f78921604fd4e5a188777e9a35c12e0d39d0f3
chh817 <chh817@gmail.com>
2022-07-01 00:11:55 +0800

    feat: add index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
-
-
+router.get('/', (req, res) => {
+  console.log(req)
+})
 
 module.exports = router
