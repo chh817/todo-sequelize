8ece21b6214fed760acfce48b2606fa37cb02da1
chh817 <chh817@gmail.com>
2022-07-01 01:17:38 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 const app = express()
+const usePassport = require('./config/passport')
 const PORT = 3000 || process.env.PORT
 const db = require('./models')
 const Todo = db.Todo
@@ -15,13 +15,25 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
-app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+
+usePassport(app)
+
 app.use(express.urlencoded({ extended: true }))
+
 app.use(methodOverride('_method'))
 
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 app.use(routes)
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const User = db.User
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: email }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
